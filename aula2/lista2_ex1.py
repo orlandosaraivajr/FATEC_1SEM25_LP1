f4130e3486524c5f85ae7aee48dd1d86df755369
Orlando Saraiva Jr <orlandosaraivajr@gmail.com>
2025-03-07 11:20:15 -0300

    Primeiro exercício da lista

---
+++ aula2/lista2_ex1.py
@@ -0,0 +1,11 @@
+'''
+Crie três variáveis em Python:
+• Uma variável chamada nome para armazenar seu nome.
+• Uma variável chamada idade para armazenar sua idade.
+• Uma variável chamada altura para armazenar sua altura em metros.
+'''
+nome = input("Digite seu nome: ")
+idade = int(input("Digite sua idade: "))
+altura = input("Digite sua altura em metros ")
+idade = str(idade)
+print("Meu nome é " + nome + ", tenho " + idade + " anos e minha altura é " + altura + " metros. ")
