fd0c0ebb8697e7920856876f011a9ea9ab8475f9
Orlando Saraiva Jr <orlandosaraivajr@gmail.com>
2025-03-14 10:36:39 -0300

    Par ou ìmpar v.1

---
+++ aula3/par_ou_impar.py
@@ -0,0 +1,7 @@
+numero = int(input("Digite um número inteiro:  "))
+
+if numero % 2 == 0:
+    print("O número " + str(numero) + " é par")
+
+if numero % 2 == 1:
+    print("O número " + str(numero) + " é impar")
